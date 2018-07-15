f1d5801bdab98c6c6c0cfa6ab5b9d33a5cad6e1c
MothershipRick <perez.ricky@gmail.com>
2018-07-15 10:52:11 -0500

    Moved logic to components jsk files.

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './OGILogo.png';
+import logo from './logo.svg';
 import './App.css';
+import Board from './components/Board.jsx';
 
 
 class App extends Component {
@@ -14,13 +15,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <div id="addPlayers"> </div>
+        <Board />
       </div>
     );
   }
 }
-
-
-
 
 export default App;
